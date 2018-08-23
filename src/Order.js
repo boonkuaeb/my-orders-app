32689a6180468c9b8219744d1d0e910daedc61f8
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-08-23 20:42:54 +0700

    int

--- src/Order.js
+++ src/Order.js
@@ -23,9 +23,6 @@ class Order extends Component {
                         order_data: result
                     });
                 },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
                 (error) => {
                     this.setState({
                         isLoaded: true,
